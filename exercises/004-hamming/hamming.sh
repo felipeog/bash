010b070dbcecb986dbc8b600e239a1e43f3fe957
felipeog <17603069+felipeog@users.noreply.github.com>
2024-03-28 19:33:15 -0300

    Add exercise 004

---
+++ exercises/004-hamming/hamming.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: hamming.sh <string1> <string2>"
+  exit 1
+fi
+
+if [ ${#1} -ne ${#2} ]
+then
+  echo "strands must be of equal length"
+  exit 1
+fi
+
+length=${#1}
+count=0
+i=0
+
+while [ $i -lt $length ]
+do
+  [ "${1:$i:1}" != "${2:$i:1}" ] && (( count++ ))
+  i=$(( i + 1 ))
+done
+
+echo $count
