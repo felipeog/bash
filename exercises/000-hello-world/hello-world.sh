689d49af3a63029dbddc16b98d0b007e7625b5d8
felipeog <17603069+felipeog@users.noreply.github.com>
2024-03-29 14:03:46 -0300

    Add references

--- exercises/000-hello-world/hello-world.sh
+++ exercises/000-hello-world/hello-world.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# echo https://www.gnu.org/software/bash/manual/bash.html#index-echo
+
 echo "Hello, World!"
--- exercises/005-acronym/acronym.sh
+++ exercises/005-acronym/acronym.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# -lt                          https://www.gnu.org/software/bash/manual/bash.html#Bash-Conditional-Expressions
+# (( ))                        https://www.gnu.org/software/bash/manual/bash.html#Shell-Arithmetic
+# [ ]                          https://www.gnu.org/software/bash/manual/bash.html#index-test
+# [...]                        https://www.gnu.org/software/bash/manual/bash.html#Pattern-Matching
+# [n] <<< word                 https://www.gnu.org/software/bash/manual/bash.html#Here-Strings
+# +=                           https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameters
+# ${#name[subscript]}          https://www.gnu.org/software/bash/manual/bash.html#Arrays
+# ${parameter:offset:length}   https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
+# ${parameter//pattern/string} https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
+# ${parameter^^pattern}        https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
+# $parameter                   https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
+# echo                         https://www.gnu.org/software/bash/manual/bash.html#index-echo
+# id++                         https://www.gnu.org/software/bash/manual/bash.html#Shell-Arithmetic
+# IFS                          https://www.gnu.org/software/bash/manual/bash.html#index-IFS
+# name=[value]                 https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameters
+# read                         https://www.gnu.org/software/bash/manual/bash.html#index-read
+# while                        https://www.gnu.org/software/bash/manual/bash.html#index-while
+
 IFS=" "
 read -r -a words <<< "${1//[^a-zA-Z\']/ }"
 
