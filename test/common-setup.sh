25452552288c5abac28c8a5173b011e18824f5f0
felipeog <17603069+felipeog@users.noreply.github.com>
2024-04-01 10:17:52 -0300

    Update workflow

--- test/common-setup.sh
+++ test/common-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 common_setup() {
-  load "../../test/bats-support/load"
-  load "../../test/bats-assert/load"
+  load "../../test/bats-support/load.bash"
+  load "../../test/bats-assert/load.bash"
 }
